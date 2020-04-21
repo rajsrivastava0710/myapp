aa39680d03b41df4ddbd38d28fabf1059b05ffc3
Raj <rajsriv.14@gmail.com>
2020-04-21 20:16:22 +0530

    Updates

---
+++ precache-manifest.84f28b5ed846823eab1afc3f751d671a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfacbad2691ad4afbc0acd7276c73e4",
+    "url": "/Demo-React-App/index.html"
+  },
+  {
+    "revision": "dd1a49fe48c44a74c5cd",
+    "url": "/Demo-React-App/static/css/main.407f3535.chunk.css"
+  },
+  {
+    "revision": "de56d08c86e172a35463",
+    "url": "/Demo-React-App/static/js/2.287bb9f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demo-React-App/static/js/2.287bb9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1a49fe48c44a74c5cd",
+    "url": "/Demo-React-App/static/js/main.d550dbb3.chunk.js"
+  },
+  {
+    "revision": "b83861aed0d0ff9c6a43",
+    "url": "/Demo-React-App/static/js/runtime-main.25e2b3cd.js"
+  }
+]);
