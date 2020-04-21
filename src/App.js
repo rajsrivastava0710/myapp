3a7bcdac917ec6784680453dad3105fcfed24942
Raj <rajsriv.14@gmail.com>
2020-04-21 20:43:02 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
 
   componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users',{mode:'cors'})
     .then(response => response.json())
     .then(users => this.setState({monster:users}))
   }
